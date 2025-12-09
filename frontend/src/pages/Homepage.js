96d5ab1a9eb0af7caa93386ac32ecc6a6e6b6ab6
k-ghale <kabinghale888@gmail.com>
2025-12-08 19:09:55 -0500

    initialized tic-tac-toe project

---
+++ frontend/src/pages/Homepage.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+function Homepage() {
+  return (
+    <div>
+      <h1>Welcome to the Homepage</h1>
+      <p>This is the main landing page of the application.</p>
+    </div>
+  );
+}
+
+
+export default Homepage;
